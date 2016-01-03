1168a47ce42f9ab01d064379f9c463d0927dd2bd
Rodrigo Fonseca <ro.fonseca.melo@gmail.com>
2016-01-03 19:11:33 -0300

    Initial commit

---
+++ www/js/app.js
@@ -0,0 +1,48 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+  // Each tab has its own nav history stack:
+  .state('dashboard', {
+    url: '/dashboard',
+    templateUrl: 'templates/dashboard.html',
+    controller: 'DashboardController'
+  })
+  .state('chat-detail', {
+    url: '/chats/:chatId',
+    templateUrl: 'templates/chat-detail.html',
+    controller: 'ChatDetailCtrl'
+  });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/dashboard');
+
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,56 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+})
+
+.controller('DashboardController', function ($scope, $timeout) {                          
+  $scope.dashboard = {swiper: false, slider: false, activeIndexView: 0};
+  $scope.dashboard.slideTo = function (indexSlide) {
+      $scope.slideTo(indexSlide);
+  };
+
+  $scope.slideTo = function (indexSlide) {
+      $scope.swiper.slideTo(indexSlide);
+  }
+              
+  $scope.$watch('dashboard.slider', function (swiper) {
+      if (swiper) {
+          $scope.swiper = swiper;
+          
+          swiper.on('onSlideChangeStart', function (swiper) {
+              $scope.dashboard.activeIndexView = swiper.snapIndex;
+              if(!$scope.$$phase) {
+                  $scope.$apply(function () {
+                       $scope.dashboard.activeIndexView = swiper.snapIndex;   
+                  }); 
+              } else {
+                  $scope.dashboard.activeIndexView = swiper.snapIndex;
+              }
+          });
+      }
+  });
+});
